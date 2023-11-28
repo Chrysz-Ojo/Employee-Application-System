5341bd6544bc11b2714cf0949dbb3b113fc4dcb6
Chrysz-Ojo <ojooluwabukunmic@gmail.com>
2023-11-28 23:12:59 +0100

    esm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import FooterComponent from './component/FooterComponent';
+// import HeaderComponent from './component/HeaderComponent';
+// import ListEmployeeComponent from './component/ListEmployeeComponent';
+
+// function App() {
+//   return (
+//     <div>
+      
+//       <Router>
+//         <HeaderComponent/>
+//         <div className="container">
+//           <Switch>
+            
+//             <Route exact path ="/" component={ListEmployeeComponent}></Route>
+//             <Route path ="/employees"component={ListEmployeeComponent}></Route>
+            
+//           </Switch>
+//         </div>
+//         <FooterComponent/>
+//         </Router>
+        
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { Route as RouteV6 } from 'react-router-dom';
+// import FooterComponent from './component/FooterComponent';
+// import HeaderComponent from './component/HeaderComponent';
+// import ListEmployeeComponent from './component/ListEmployeeComponent';
+// import AddEmployeeComponent from './component/AddEmployeeComponent';
+
+// function App() {
+//   return (
+//     <div>
+//       <Router>
+//         <HeaderComponent />
+//         <div className="container">
+//           <Routes>
+//             <RouteV6 path="/" element={<ListEmployeeComponent />} />
+//             <RouteV6 path="/employees" element={<ListEmployeeComponent />} />
+//             <RouteV6 path="/add-employee"Component={AddEmployeeComponent} />
+//             <RouteV6 path="/edit-employee/:id" Component={AddEmployeeComponent}/>
+//           </Routes>
+//         </div>
+//         <FooterComponent />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import { BrowserRouter as Router, Routes, Route as RouteV6 } from 'react-router-dom';
+import FooterComponent from './component/FooterComponent';
+import HeaderComponent from './component/HeaderComponent';
+import ListEmployeeComponent from './component/ListEmployeeComponent';
+import AddEmployeeComponent from './component/AddEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <RouteV6 path="/" element={<ListEmployeeComponent />} />
+            <RouteV6 path="/employees" element={<ListEmployeeComponent />} />
+            <RouteV6 path="/add-employee" element={<AddEmployeeComponent />} />
+            <RouteV6 path="/edit-employee/:id" element={<AddEmployeeComponent />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
